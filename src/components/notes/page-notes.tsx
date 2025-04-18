b75e2b65f7bd8bed9ac9f544c5ae2fd66d378932
Redreev8 <redreevs@mail.ru>
2025-04-18 16:23:39 +0500

    add animate to /note

--- src/components/notes/page-notes.tsx
+++ src/components/notes/page-notes.tsx
@@ -4,14 +4,20 @@ import Container from '../ui/container'
 import useGetNotesTree from './hooks/useGetNotesTree'
 import ListNote from './list-notes'
 import useDaleyToHref from '@/hooks/useDaleyToHref'
-import { AnimationEvent, useState } from 'react'
+import { AnimationEvent, useEffect, useState } from 'react'
 const PageNotes = () => {
     const { isPending, prevList, activeList } = useGetNotesTree()
     const [isToGroup, setIsToGroup] = useState<boolean>(false)
+    const [isToNote, setIsToNote] = useState<boolean>(false)
 
     const { handelClickHref, to, toNoAnimate } = useDaleyToHref({
-        onClick: () => {
-            console.log(prevList.length)
+        onClick: (e) => {
+            const link = e.currentTarget
+            if (link.href!.match(/\/note\//)) {
+                console.log(link.href!.match(/\/note\//))
+                setIsToNote(true)
+                return
+            }
             if (prevList.length === 0) {
                 toNoAnimate()
                 return
@@ -19,24 +25,26 @@ const PageNotes = () => {
             setIsToGroup(true)
         },
         cbTo: () => {
-            setTimeout(() => {
-                setIsToGroup(false)
-            }, 1000)
+            setIsToGroup(false)
         },
     })
+    useEffect(() => {
+        console.log(isToNote)
+    }, [isToNote])
     const handelAnimateEnd = (e: AnimationEvent<HTMLDivElement>) => {
         if (e.target !== e.currentTarget) return
-        if (!isToGroup) return
-        to()
+        if (isToGroup || isToNote) to()
     }
-    const clActive = classNames('animate-show-delay bg-bg w-full opacity-0', {
+    const clActive = classNames('bg-bg w-full opacity-0', {
         'max-w-[50%]': prevList.length !== 0,
         'max-w-[calc(50%-10px)]': prevList.length === 0,
+        'animate-hide': isToNote,
+        'animate-show-delay': !isToNote,
     })
     const clPrev = classNames('bg-bg z-2 w-full max-w-[50%]', {
-        'animate-hide': isToGroup,
+        'animate-hide': isToGroup || isToNote,
         'animate-right-to-left transform-(--animate-right-to-left-start-transform)':
-            !isToGroup,
+            !isToGroup && !isToNote,
     })
     if (isPending) return
     return (
@@ -49,7 +57,7 @@ const PageNotes = () => {
                     />
                 </div>
             )}
-            <div className={clActive}>
+            <div onAnimationEnd={handelAnimateEnd} className={clActive}>
                 <ListNote onToGroupOrNote={handelClickHref} list={activeList} />
             </div>
         </Container>
