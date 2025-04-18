6a31f12fe399004955bcf3a472a5a53a10351973
Redreev8 <redreevs@mail.ru>
2025-04-18 17:29:24 +0500

    add api notes/content

---
+++ src/app/api/notes/content/[[...path]]/route.ts
@@ -0,0 +1,34 @@
+import sql from '@/configs/sql'
+import { nameTableNoteContent } from '@/configs/sql/create-table-note-content'
+
+interface NoteContentSql {
+    content: string
+    path: string
+}
+
+export const GET = async (
+    request: Request,
+    { params }: { params: Promise<{ path: string[] }> },
+) => {
+    try {
+        const { path } = await params
+        console.log()
+        const [noteContent] = (await sql.query(
+            `
+            SELECT *
+            FROM ${nameTableNoteContent} 
+            WHERE path = $1`,
+            [path.join('.')],
+        )) as unknown as NoteContentSql[]
+        if (!noteContent) {
+            return new Response('not found', {
+                status: 404,
+            })
+        }
+        return new Response(noteContent.content)
+    } catch {
+        return new Response('error server', {
+            status: 500,
+        })
+    }
+}
