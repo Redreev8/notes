e78b766e588d077cda2c5305bcdc269a24fa4000
Redreev8 <redreevs@mail.ru>
2025-04-22 20:48:57 +0500

    fix useDaleyToHref

---
+++ src/hooks/useDaleyToHref.tsx
@@ -0,0 +1,41 @@
+import { useRouter } from 'next/navigation'
+import { MouseEvent, useEffect, useState } from 'react'
+
+interface useDaleyToHrefProps {
+    onClick?: (e: MouseEvent<HTMLAnchorElement>) => void
+    cbTo?: () => void
+}
+
+const useDaleyToHref = ({ onClick, cbTo }: useDaleyToHrefProps = {}) => {
+    const [href, setHref] = useState<string | null>(null)
+    const [noAnimate, setNoAnimate] = useState<boolean>(false)
+    const { push } = useRouter()
+    const handelClickHref = (e: MouseEvent<HTMLAnchorElement>) => {
+        e.preventDefault()
+        const link = e.currentTarget
+        setHref(() => link.href)
+
+        if (onClick) onClick(e)
+    }
+    const to = async () => {
+        console.log(href)
+        if (!href) return
+        push(href)
+        setHref(null)
+        if (cbTo) cbTo()
+    }
+    const toNoAnimate = () => setNoAnimate(true)
+    useEffect(() => {
+        if (!href || !noAnimate) return
+        push(href)
+        setHref(null)
+        setNoAnimate(false)
+    }, [href, noAnimate])
+    return {
+        handelClickHref,
+        to,
+        toNoAnimate,
+    }
+}
+
+export default useDaleyToHref
